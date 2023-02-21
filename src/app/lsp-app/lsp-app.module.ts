4c886c5b045329724447e6ee85af9919db17c75c
steingartpeter <steingart.p2@gmail.com>
2023-02-21 09:19:42 +0100

    2023-02-21 001 HAL

---
+++ src/app/lsp-app/lsp-app.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { LspAppRoutingModule } from './lsp-app-routing.module';
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    LspAppRoutingModule
+  ]
+})
+export class LspAppModule { }
